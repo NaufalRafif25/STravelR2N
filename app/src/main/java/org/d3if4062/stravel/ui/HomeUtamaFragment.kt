298b0ff57aff66f7ed2d78616287ef09973d8da5
Naufal_Rafif <naufalrafifc@student.telkomuniversity.ac.id>
2021-05-05 04:51:29 +0700

    test app fragment binding masih error

---
+++ app/src/main/java/org/d3if4062/stravel/ui/HomeUtamaFragment.kt
@@ -0,0 +1,18 @@
+package org.d3if4062.stravel.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.d3if4062.stravel.databinding.FragmentHomeUtamaBinding
+
+class HomeUtamaFragment : Fragment() {
+    private lateinit var binding: FragmentHomeUtamaBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeUtamaBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/org/d3if4062/stravel/ui/RegisterActivity.kt
+++ app/src/main/java/org/d3if4062/stravel/ui/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package org.d3if4062.stravel
+package org.d3if4062.stravel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if4062.stravel.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.fragment_register)
     }
 }
