bd2099b66d94ff60208c046c37b6ca7def91caf3
Naufal_Rafif <naufalrafifc@student.telkomuniversity.ac.id>
2021-05-05 05:48:25 +0700

    test app fragment binding masih error & test fragment Register

--- app/src/main/java/org/d3if4062/stravel/ui/RegisterFragment.kt
+++ app/src/main/java/org/d3if4062/stravel/ui/RegisterFragment.kt
@@ -1,12 +1,17 @@
 package org.d3if4062.stravel.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.d3if4062.stravel.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.d3if4062.stravel.databinding.FragmentRegisterBinding
 
-class RegisterFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_register)
+class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
