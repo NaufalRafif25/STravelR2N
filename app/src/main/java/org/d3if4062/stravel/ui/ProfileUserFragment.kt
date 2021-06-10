d0209837c0cf71766d061a1c416f7f9ea48ddc17
Naufal_Rafif <naufalrafifc@student.telkomuniversity.ac.id>
2021-06-10 08:55:55 +0700

    ui profile user

---
+++ app/src/main/java/org/d3if4062/stravel/ui/ProfileUserFragment.kt
@@ -0,0 +1,8 @@
+package org.d3if4062.stravel.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4062.stravel.databinding.FragmentProfileUserBinding
+
+class ProfileUserFragment : Fragment() {
+    private lateinit var binding: FragmentProfileUserBinding
+}
