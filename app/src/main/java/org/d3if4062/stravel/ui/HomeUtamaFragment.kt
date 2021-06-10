d713393532a70990eab2a49cb81ba311864c0fe7
Naufal_Rafif <naufalrafifc@student.telkomuniversity.ac.id>
2021-06-10 12:21:58 +0700

    benerin UI home / ui profile user dan penyesuaian navigation

--- app/src/main/java/org/d3if4062/stravel/ui/HomeUtamaFragment.kt
+++ app/src/main/java/org/d3if4062/stravel/ui/HomeUtamaFragment.kt
@@ -2,11 +2,10 @@ package org.d3if4062.stravel.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if4062.stravel.R
 import org.d3if4062.stravel.databinding.FragmentHomeUtamaBinding
 
@@ -19,6 +18,23 @@ class HomeUtamaFragment : Fragment() {
         binding.button.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_homeUtamaFragment_to_homeHotelFragment) }
         binding.button2.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_homeUtamaFragment_to_homePaketWisataFragment) }
         binding.button3.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_homeUtamaFragment_to_homeTiketPesawatFragment) }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.profile) {
+            findNavController().navigate(
+                    R.id.action_homeUtamaFragment_to_profileUserFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
--- app/src/main/java/org/d3if4062/stravel/ui/ProfileUserFragment.kt
+++ app/src/main/java/org/d3if4062/stravel/ui/ProfileUserFragment.kt
@@ -1,8 +1,9 @@
 package org.d3if4062.stravel.ui
 
 import androidx.fragment.app.Fragment
+import org.d3if4062.stravel.R
 import org.d3if4062.stravel.databinding.FragmentProfileUserBinding
 
-class ProfileUserFragment : Fragment() {
+class ProfileUserFragment : Fragment(R.layout.fragment_profile_user) {
     private lateinit var binding: FragmentProfileUserBinding
 }
